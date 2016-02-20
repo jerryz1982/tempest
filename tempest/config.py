41df29ce033830070e24409161dfb5828e7b385b
Jerry Zhao <xyzjerry@gmail.com>
2016-02-19 17:00:24 -0800

    increase ping timeout to allow for weak test machines.

--- tempest/config.py
+++ tempest/config.py
@@ -616,7 +616,7 @@ ValidationGroup = [
                default=4,
                help='Default IP version for ssh connections.'),
     cfg.IntOpt('ping_timeout',
-               default=120,
+               default=300,
                help='Timeout in seconds to wait for ping to succeed.',
                deprecated_opts=[cfg.DeprecatedOpt('ping_timeout',
                                                   group='compute')]),
